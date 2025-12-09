7bfb2b3cc6beb25fd8c80f65ab21c7e1910ab1fa
Conor Schaefer <conor@freedom.press>
2025-12-09 10:45:39 -0800

    fix: switch to sdw dir for makefile target

--- tests/securedrop/test_dom0.pm
+++ tests/securedrop/test_dom0.pm
@@ -31,7 +31,7 @@ sub run {
     my $sdw_path = "/home/user/securedrop-workstation";
 
     # Setup testing requirements and run tests
-    assert_script_run('make install-dom0-test-prereqs', timeout => 300);
+    assert_script_run("make -C $sdw_path install-dom0-test-prereqs", timeout => 300);
 
     assert_script_run("env XAUTHORITY=/run/lightdm/user/xauthority DISPLAY=:0.0 CI=true make -C $sdw_path test | tee make-test.log", timeout => 2400);
 
